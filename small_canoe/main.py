e8db89ad0fac192af115913bf874d3d48390eeab
Anthony <sean.a.protho1@gmail.com>
2021-04-12 15:11:38 -0700

    done with this

---
+++ small_canoe/main.py
@@ -0,0 +1,15 @@
+# the boat can only carry 100kg
+boat = 100
+
+# we need to get these things accross the river
+alex = 90
+brook = 80
+chris = 60
+dusty = 40
+supplies = 20
+
+# Chris and Dusty row across, Dusty returns.
+# Alex rows over, and Chris returns.
+# Chris and Dusty row across again, Dusty returns.
+# Brook rows across with the Supplies, and Chris returns.
+# Chris and Dusty row across again for the last time.
---
+++ which_ball/main.py
@@ -0,0 +1,13 @@
+# split the balls into 3 groups
+# 1 will have 2 balls and the others will have 3
+# we then compare 3 v 3 on our first use of the scale
+
+# if the 2 groups of 3 weigh the same, we can then use our
+# second use of the scale to compare the two balls in the first group
+# whichever is heavier is our solution
+
+# else if one group of 3 weighs more, than we will
+# use our second use of the scale to compare two of the 3 balls
+# if the two weigh the same, than the one not being measured is our answer
+# elif one weighs more than another. Then the one that weighs more is 
+# our solutions.
